810e1e975d4bfc01a968ca4dc7d58589d8a1328f
Anastasiya Scshekotina <lenavl@list.ru>
2023-09-28 14:28:01 +0700

    Initial commit

---
+++ src/main/java/com/example/demo/MtsStepikCourseApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtsStepikCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtsStepikCourseApplication.class, args);
+		Logger logger = LoggerFactory.getLogger(MtsStepikCourseApplication.class);
+		logger.trace("trace");
+		logger.debug("debug");
+		logger.info("info");
+		logger.warn("warn");
+		logger.error("error");
+
+	}
+
+
+}
